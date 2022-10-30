955a490fe960836be7e4903c3ccb15c231e6d418
algemorge <mafia18@naver.com>
2022-10-31 00:03:34 +0900

    js 명명 규칙

---
+++ Javascript/day1/day5.js
@@ -0,0 +1,13 @@
+// js에서 변수명명과 규칙
+
+/** 엄격한 규칙
+ * 변수명에 공백 불가 - user name
+ * 숫자로 시작 불가 - 1user
+ * 식별자는 문자, $, _, 0~9 가능
+ */
+
+/** 통상적인 규칙
+ * camelCase 공백이 생기려는 곳에 대문자 사용
+ * python은 snake_case 사용
+ * is를 붙이면 boolean임을 암시 - let isGameOver true;
+ */
