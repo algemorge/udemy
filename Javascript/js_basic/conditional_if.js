c3fdcc55c549298271cbd6beb5577be9010065db
algemorge <mafia18@naver.com>
2022-11-11 22:49:53 +0900

    js_basic

---
+++ Javascript/js_basic/conditional_if.js
@@ -0,0 +1,28 @@
+/****************************************** if ******************************************************/
+// 점수가 3점일때만 실행되는 조건문
+let rating = 3;
+if (rating === 3) {
+   console.log("YOU ARE A SUPERSTAR!!");
+}
+
+// 그녀의 차가 소나타일때만 실행되는 조건문
+let herCar = "sonata"
+if (herCar === "sonata") {
+   console.log("buy a new car")
+} // buy a new car
+
+let random = Math.random()
+if (random < 0.5) {
+   console.log("this is less than 0.5");
+   console.log(random);
+} // 50% 확률로 출력됨
+if (random >= 0.5) {
+   console.log("this is greater than 0.5");
+   console.log(random);
+
+}
+
+// 조건이 틀리면 실행되지 않음
+if (1 + 1 === "2") {
+   console.log("math still works!");
+}
---
+++ Javascript/js_basic/evaluate.js
@@ -0,0 +1,8 @@
+// 브라우저는 코드를 저장하기 어렵다-> 편집기 사용하기 vscode
+// html 파일에 js 파일을 넣기 위해서 script 태그 사용 
+// body 태그의 아래에 script 태그 넣기 추천
+console.log("Hello from our first js file!");
+
+// 여전히 console에 뜨지 않지만 변수로 접근가능
+let total = 1 + 5;
+console.log("goodbye");
