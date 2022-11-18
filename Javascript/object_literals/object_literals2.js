64c9c6fc140fd6e5b12ed8e2188d11609df5dddb
algemorge <mafia18@naver.com>
2022-11-18 22:35:51 +0900

    object literals

---
+++ Javascript/object_literals/object_literals2.js
@@ -0,0 +1,28 @@
+/*************************************** object literals *******************************************/
+/** 객체 리터럴
+ * JS에서 객체는 여러 의미가 담긴 용어이기 때문에 객체리터럴이라 부른다.
+ * 객체를 만들면 그 안에 온갖 종류의 데이터를 값으로 저장 가능. 
+ * 값은 콜론의 오른족에 온다.
+ * 
+ */
+
+const person = {
+   fistName: 'Naomi',
+   lastName: 'Scott'
+}
+
+const comment = {
+   username: 'tonystark',
+   downVotes: 2340,
+   upVotes: 1093,
+   netScore: 1924,
+   commentText: 'I am Iron Man',
+   tags: ['#hero', '#starkindustry', '#war'],
+   isGillded: false
+};
+
+const kitchenSink = {
+   favNum: 239849,
+   isFunny: true,
+   colors: ['red', 'orange']
+};
